2d504eef4bb889b1927ebab7c179e3927af8b602
lequochaidevh <lequochaidevh@gmail.com>
2024-03-14 04:40:34 +0700

    code show plot chart

--- src/networkplot.cpp
+++ src/networkplot.cpp
@@ -6,6 +6,28 @@ NetworkPlot::NetworkPlot(QWidget *parent)
     , ui(new Ui::NetworkPlot)
 {
     ui->setupUi(this);
+
+
+    ui->customPlot->addGraph();
+    ui->customPlot->graph(0)->setScatterStyle(QCPScatterStyle::ssCircle);
+    ui->customPlot->graph(0)->setLineStyle(QCPGraph::lsLine);
+    ui->customPlot->xAxis->setLabel("X");
+    ui->customPlot->yAxis->setLabel("Y");
+    ui->customPlot->xAxis->setRange(-6000,100);
+    ui->customPlot->yAxis->setRange(-6000,8000);
+
+    ui->customPlot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectPlottables);
+
+    QVector<double> x = {1,2,3,4,5},y={4,6,8,2,5};
+
+    ui->customPlot->graph(0)->setData(x,y);
+    ui->customPlot->rescaleAxes();
+    ui->customPlot->replot();
+    ui->customPlot->update();
+
+    //ui->plotTypeComboBox->setCurrentIndex(4);
+    //ui->lineStyleComboBox->setCurrentIndex(1);
+
 }
 
 NetworkPlot::~NetworkPlot()
--- src/qcustomplot.cpp
+++ src/qcustomplot.cpp
@@ -23,7 +23,7 @@
 **          Version: 2.1.1                                                **
 ****************************************************************************/
 
-#include "../include/qcustomplot.h"
+#include "qcustomplot.h"
 
 
 /* including file 'src/vector2d.cpp'       */
